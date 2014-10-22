81da4970ddde0961390d26dde709240b2775a808
Michael Stock <mikeastock@gmail.com>
2014-10-21 21:00:49 -0700

    A bunch of changes

--- player.rb
+++ player.rb
@@ -14,7 +14,13 @@ class Player
   end
 
   def position
-    position_text.match(/(QB|RB|WR|TE|D\/ST|K)/).captures.first
+    position_text.
+      match(/(QB|RB|WR|TE|D\/ST|K)/).
+      captures.
+      first.
+      downcase.
+      gsub("/", "_").
+      to_sym
   end
 
   def present?
@@ -22,7 +28,15 @@ class Player
   end
 
   def non_zero?
-    points != 0
+    if position == :d_st || position == :k
+      true
+    else
+      points != 0
+    end
+  end
+
+  def name
+    player_name_element.split(",").first
   end
 
   private
@@ -30,6 +44,10 @@ class Player
   attr_reader :element
 
   def position_text
-    element.at_css(".playertablePlayerName").text.split(" ").last 
+    player_name_element.split(" ").last 
+  end
+
+  def player_name_element
+    element.at_css(".playertablePlayerName").text
   end
 end
--- team.rb
+++ team.rb
@@ -1,13 +1,16 @@
+require "pry"
 require "httparty"
 require "nokogiri"
 require_relative "player"
 
 class Team
   attr_reader :page, :players
+  attr_accessor :available_players
 
   def initialize(url)
     @page = setup_page(url)
     @players = setup_players
+    @available_players = grouped_players
   end
 
   def total_points
@@ -18,17 +21,58 @@ class Team
     bench_players.map(&:points).inject(0) { |sum, points| sum + points }
   end
 
-  def woulda
-    score = 0
-    score += lowest_players_scores(position: "QB", number: 1)
-    score += lowest_players_scores(position: "RB", number: 2)
-    score += lowest_players_scores(position: "WR", number: 3)
-    score += lowest_players_scores(position: "TE", number: 1)
-    score += lowest_players_scores(position: "D/ST", number: 1)
-    score += lowest_players_scores(position: "K", number: 1)
+  def woulda_team
+    @woulda_team ||= setup_woulda_team_team
   end
 
-  private
+  def setup_woulda_team_team
+    team = ActiveTeam.new
+    team.qb = lowest_players(position: :qb, number: 1)
+    team.rb = lowest_players(position: :rb, number: 2)
+    team.wr = lowest_players(position: :wr, number: 3)
+    team.te = lowest_players(position: :te, number: 1)
+    team.d_st = lowest_players(position: :d_st, number: 1)
+    team.k = lowest_players(position: :k, number: 1)
+    team
+  end
+
+  def coulda
+    total_points
+  end
+
+  def name
+    page.at_css(".team-name").text
+  end
+
+  # private
+  
+  def calculate_woulda
+    score = 0
+    score += lowest_players_scores(position: :qb, number: 1)
+    score += lowest_players_scores(position: :rb, number: 2)
+    score += lowest_players_scores(position: :wr, number: 3)
+    score += lowest_players_scores(position: :te, number: 1)
+    score += lowest_players_scores(position: :d_st, number: 1)
+    score += lowest_players_scores(position: :k, number: 1)
+    score += lowest_flex_score(number: 1)
+  end
+
+  def lowest_flex_score(args)
+    number = args.fetch(:number)
+
+    player = available_flex_players.sort_by(&:points).first
+    player.points
+  end
+
+  def flex_positions
+    [:rb, :wr, :te]
+  end
+
+  def available_flex_players
+    @available_flex_players ||= flex_positions.inject(Array.new) do |players, position|
+      players.push(available_players[position])
+    end.flatten
+  end
 
   def grouped_players
     @grouped_players ||= non_zero_players.group_by(&:position)
@@ -40,7 +84,17 @@ class Team
 
   def lowest_players_scores(args)
     players = lowest_players(args)
+
+    update_available_players(args.merge(players: players))
     players.map(&:points).inject(0) { |sum, points| sum + points }
+  end
+
+  def update_available_players(args)
+    players = args.fetch(:players)
+    position = args.fetch(:position)
+    
+    new_available_players = available_players[position] - players
+    self.available_players[position] = new_available_players
   end
 
   def lowest_players(args)
@@ -71,3 +125,70 @@ class Team
   end
 end
 
+class ActiveTeam
+  attr_accessor :players
+
+  def initialize
+    @players = setup_players
+  end
+
+  def qb
+    players[:qb]
+  end
+
+  def qb=(value)
+    self.players[:qb] = value
+  end
+
+  def rb
+    players[:rb]
+  end
+
+  def rb=(value)
+    self.players[:rb] + value
+  end
+
+  def wr
+    players[:wr]
+  end
+
+  def wr=(value)
+    self.players[:wr] + value
+  end
+
+  def te
+    players[:te]
+  end
+
+  def te=(value)
+    self.players[:te] = value
+  end
+
+  def flex
+    players[:flex]
+  end
+
+  def flex=(value)
+    self.players[:flex] = value
+  end
+
+  def k
+    players[:k]
+  end
+
+  def k=(value)
+    self.players[:k] = value
+  end
+
+  def setup_players
+    {
+      qb: "",
+      rb: [],
+      wr: [],
+      te: "",
+      flex: "",
+      d_st: "",
+      k: ""
+    }
+  end
+end
